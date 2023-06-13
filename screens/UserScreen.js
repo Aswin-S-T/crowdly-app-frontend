8a4ca423f467f6592e52aff0f0ce1f215c27952d
Aswin-S-T <aswinswebart15@gmail.com>
2023-06-13 20:55:53 +0530

    user screen api integrated

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,175 +1,102 @@
 import {
-	Text,
-	SafeAreaView,
-	View,
-	FlatList,
-	StyleSheet,
-	ScrollView,
-	StatusBar,
-	Image,
+  Text,
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  ScrollView,
+  StatusBar,
+  Image,
+  Button,
 } from "react-native";
-import React from "react";
-
-const DATA = [
-	{
-		id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-		title: "First Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-		title: "Second Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "58694a0f-3da1-471f-bd96-145571e29d72",
-		title: "Third Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-		title: "First Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-		title: "Second Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "58694a0f-3da1-471f-bd96-145571e29d72",
-		title: "Third Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-		title: "First Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-		title: "Second Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "58694a0f-3da1-471f-bd96-145571e29d72",
-		title: "Third Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-		title: "First Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-		title: "Second Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "58694a0f-3da1-471f-bd96-145571e29d72",
-		title: "Third Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-		title: "First Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-		title: "Second Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-	{
-		id: "58694a0f-3da1-471f-bd96-145571e29d72",
-		title: "Third Item",
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1JORCag4QA5k8Hk3T4qVNZHNpnJPG5xg6InmPYyvuajMleITpa_eX-YG_Lw2PiEyG0qs&usqp=CAU",
-	},
-];
-
-const Item = ({ image, title }) => (
-	<View style={{ flexDirection: "row", alignItems: "center" }}>
-		<Image
-			source={{ uri: image }}
-			style={{ width: 50, height: 50, marginRight: 10 }}
-		/>
-		<Text>{title}</Text>
-	</View>
-);
-const Separator = () => {
-	return <View style={{ height: 10, backgroundColor: "transparent" }} />;
-};
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../constants/api";
 
 const UserScreen = () => {
-	const renderItem = ({ item }) => (
-		<Item image={item.image} title={item.title} />
-	);
-	return (
-		<ScrollView>
-			<SafeAreaView style={styles.container}>
-				{/* <FlatList
-					data={DATA}
-					renderItem={({ item }) => <Item title={item.title} />}
-					keyExtractor={(item) => item.id}
-				/> */}
-				<Text style={styles.heading}>All Users</Text>
-				<FlatList
-					style={styles.list}
-					data={DATA}
-					renderItem={renderItem}
-					keyExtractor={(item) => item.id}
-					ItemSeparatorComponent={Separator}
-				/>
-			</SafeAreaView>
-		</ScrollView>
-	);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${BACKEND_URL}/api/v1/user/all-users`)
+      .then((response) => {
+        setUsers(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const sendFollow = async () => {};
+
+  return (
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+        {loading ? (
+          <Text>Loading....</Text>
+        ) : (
+          users.map((user) => (
+            <View style={styles.postHeader}>
+              <Image
+                style={styles.thumb}
+                source={{
+                  uri: user.profileImage,
+                }}
+              />
+              <Text>{user?.username}</Text>
+              <Button title="Follow+" onPress={sendFollow} />
+            </View>
+          ))
+        )}
+      </SafeAreaView>
+    </ScrollView>
+  );
 };
 
 export default UserScreen;
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: StatusBar.currentHeight || 0,
-		padding: 15,
-	},
-	item: {
-		backgroundColor: "blue",
-		padding: 4,
-		marginVertical: 5,
-		marginTop: 10,
-		borderRadius: 50,
-		margin: 20,
-	},
-	title: {
-		fontSize: 22,
-	},
-	image: {
-		borderRadius: 50,
-	},
-	list: {
-		padding: 5,
-		marginTop: 10,
-		top: 30,
-	},
-	heading: {
-		fontSize: 25,
-		fontWeight: 600,
-		margin: 20,
-	},
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    padding: 15,
+  },
+  item: {
+    backgroundColor: "blue",
+    padding: 4,
+    marginVertical: 5,
+    marginTop: 10,
+    borderRadius: 50,
+    margin: 20,
+  },
+  title: {
+    fontSize: 22,
+  },
+  image: {
+    borderRadius: 50,
+  },
+  list: {
+    padding: 5,
+    marginTop: 10,
+    top: 30,
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: 600,
+    margin: 20,
+  },
+  thumb: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+  },
+  postHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flex: 1,
+    backgroundColor: "white",
+    padding: 15,
+  },
 });
